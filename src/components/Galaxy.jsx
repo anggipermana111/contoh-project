7227043acf8e765a77544ebdd28c2fd1585d1855
Hanafi Abdullah <hanafikonten@gmail.com>
2023-06-12 21:36:52 +0700

    Anggi 5

--- src/components/Galaxy.jsx
+++ src/components/Galaxy.jsx
@@ -10,24 +10,26 @@ const Galaxy = (props) => {
   const [suka,setSuka] = useState(false);
   const handleClick2 = () => {
     setSuka(!suka)
+    setWarna(!warna)
   };
+  const [warna,setWarna] = useState(false);
   return (
     <figure key={props.id}>
         <img src={props.gambar} alt={props.nama} />
         <figcaption>
         <h1>{props.nama}</h1>
         <p>{props.diameter}</p>
-        </figcaption>
-        <button onClick={handleClick} >selengkapnya</button>
-        <button onClick={handleClick2} >
-          {
-            suka?"batal suka":"suka"
-          }
-        </button>
         <hr />
         {
           desk?<p>{props.deskripsi}</p>:<p></p>
         }
+        </figcaption>
+        <button onClick={handleClick} >selengkapnya</button>
+        <button onClick={handleClick2} style={{ backgroundColor:warna?"aqua":"pink" }} >
+          {
+            suka?"batal suka":"suka"
+          }
+        </button>
     </figure>
   )
 }
